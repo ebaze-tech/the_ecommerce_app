d30a1b4b33784351417a41b172e10ace421cb73c
ebaze-tech <ebaze.technologies@gmail.com>
2025-02-06 13:21:00 +0100

    Initial commit of the_ecommerce_app

---
+++ lib/models/cart.dart
@@ -0,0 +1,38 @@
+import 'shoe.dart';
+
+class Cart {
+  // list of shoes for sale
+  List<Shoe> shoeShop = [
+    Shoe(
+      name: 'Air Jordan',
+      price: '236',
+      imagePath: 'lib/images/AirJordanOne.jpg',
+      description: 'The forward-thinking design of his latest signature shoe.',
+    ),
+    Shoe(
+      name: 'All Star',
+      price: '245',
+      imagePath: 'lib/images/AllStar.jpg',
+      description: 'The shoe made for comfort',
+    ),
+    Shoe(
+      name: 'Black Nike',
+      price: '205',
+      imagePath: 'lib/images/BlackNike.jpg',
+      description: "One of Nike's best shoes.",
+    ),
+    Shoe(
+      name: 'Air Jordan',
+      price: '236',
+      imagePath: 'lib/images/HighLoafer.jpg',
+      description: 'Loafers designed just for you.',
+    ),
+  ];
+  // list of items in user cart
+
+  // get list of shoes for sale
+
+  // get cart
+
+  // remove item from cart
+}
---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:e_commerce_app/models/shoe.dart';
+import 'package:e_commerce_app/components/shoe_tile.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+              color: Colors.grey[100], borderRadius: BorderRadius.circular(15)),
+          child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Search", style: TextStyle(color: Colors.grey)),
+                Icon(Icons.search, color: Colors.grey)
+              ])),
+      Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text("Everyone flies.....some fly longer than others",
+              style: TextStyle(color: Colors.grey[600]))),
+      const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text("Hot Picks ",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                Text("See all",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.blue))
+              ])),
+      const SizedBox(height: 10),
+      Expanded(
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                Shoe shoe = Shoe(
+                    name: 'Air Jordan',
+                    price: '240',
+                    description: 'Cool Shoe',
+                    imagePath: 'lib/images/AirJordanOne.jpg');
+                return ShoeTile(shoe: shoe);
+              })),
+      const Padding(
+        padding: EdgeInsets.only(top: 25, right: 25, left: 25),
+        child: Divider(
+          color: Colors.white,
+        ),
+      )
+    ]);
+  }
+}
